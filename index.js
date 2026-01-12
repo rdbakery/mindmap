deb41f50ba716881561ebb9c1687a6e11b983e1c
chintoo kumar <chintookumar@chintoos-MacBook-Air.local>
2026-01-12 16:07:21 +0530

    node color changes

--- index.js
+++ index.js
@@ -11,33 +11,36 @@ const safeName = n => (n||"mindmap").replace(/[<>:"/\\|?*]+/g,"").replace(/\s+/g
 /* ===== Node Color by Level ===== */
 function nodeColor(depth) {
   const palette = [
-       "#d1cfee", // very light blue-violet
-  "#c9dbf0", // very light pastel blue
-  "#b8d6dd", // very light blue-teal
-  "#bfe5e1", // very light pastel teal
-  "#c7e3d6", // very light pastel green
+  //      "#d1cfee", // very light blue-violet
+  // "#c9dbf0", // very light pastel blue
+  // "#b8d6dd", // very light blue-teal
 
-  "#d7f5ef", // very light mint-teal
-  "#dcf6cf", // very light leaf green
-  "#dff5ea", // very light airy mint
+  // "#c7e3d6", // very light pastel green
 
-  "#dceff9", // very light sky-blue
-  "#c8f7e5", // very light fresh green
+  // "#d7f5ef", // very light mint-teal
+  // "#dcf6cf", // very light leaf green
+  // "#dff5ea", // very light airy mint
 
-  "#ddece4", // very light sage
-  "#edf2df"  // very light sage-pastel
-  // "#afacd6", // soft blue-violet
-  // "#8bb3d9", // pastel blue
-  // "#6fa1ac", // blue-teal
-  // "#72b9b3", // pastel teal
-  // "#8fbea6", // pastel green
-  // "#a8e2d6", // mint-teal
-  // "#b3e2a2",  // leaf green
-  // "#b8e4cb", // airy mint
-  // "#b3e1f2", // airy sky-blue
-  // "#8aeac1", // fresh light green
-  // "#b7d6c6", // soft sage
-  // "#ccd9b7", // sage pastel
+  // "#dceff9", // very light sky-blue
+  // "#c8f7e5", // very light fresh green
+
+  // "#ddece4", // very light sage
+  // "#edf2df",  // very light sage-pastel
+  //   "#bfe5e1" // very light pastel teal
+  "#bcb9ed", // soft blue-violet
+  "#a4c9eb", // pastel blue
+  "#8fbea6", // pastel green
+  "#96b8bf", // blue-teal
+  "#a8e2d6", // mint-teal
+  "#72b9b3", // pastel teal
+  
+  
+  "#b3e2a2",  // leaf green
+  "#b8e4cb", // airy mint
+  "#b3e1f2", // airy sky-blue
+  "#8aeac1", // fresh light green
+  "#b7d6c6", // soft sage
+  "#ccd9b7", // sage pastel
 
 ];
 
